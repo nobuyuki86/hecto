004edfc9aca889d95bff2886667307c6d8973724
Nobuyuki Saito <nobuyuki86@outlook.jp>
2021-02-15 22:06:53 +0900

    ch6 Search forward and backward.

--- src/document.rs
+++ src/document.rs
@@ -105,11 +105,13 @@ impl Document {
         self.dirty
     }
 
-    pub fn find(&self, query: &str) -> Option<Position> {
-        for (y, row) in self.rows.iter().enumerate() {
+    pub fn find(&self, query: &str, after: &Position) -> Option<Position> {
+        let mut x = after.x;
+        for (y, row) in self.rows.iter().enumerate().skip(after.y) {
             if let Some(x) = row.find(query) {
                 return Some(Position { x, y });
             }
+            x = 0;
         }
         None
     }
